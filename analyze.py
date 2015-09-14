b29837c6792d1cda94c0d36872e47bdb792eecb0
kartanis <kartanisoid@gmail.com>
2015-09-15 00:00:41 +0300

    Added some functionality to load files info and save it in file.

--- analyze.py
+++ analyze.py
@@ -4,20 +4,36 @@ import sys
 
 __author__ = 'sbilyi'
 
+def loadFileInfo(path):
+    BLOCKSIZE = 65536
+    hasher = hashlib.sha1()
+    size = 0
+    with open(path, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        if len(buf) > 0: #first time we are doing has from first block to get faster results
+            size += len(buf)
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
 
-def loadFolder(path):
-    print os.path.isdir(path)
+    result = {'name': path, 'size': os.stat(path).st_size, 'sha1': hasher.hexdigest(), 'type': 'FILE'}
+
+    #print result
+    return result
+
+
+def loadFolder(path, outputFile):
+    print path
 
     files = []
     subfolders = []
 
     for afile in os.listdir(path):
         if os.path.isdir(os.path.join(path, afile)):
-            subfolders.append(afile)
-            afile += "[D]"
+            loadFolder(os.path.join(path, afile), outputFile)
+
         else:
-            files.append(afile)
-            afile += "[F]"
+            outputFile.write(str(loadFileInfo(os.path.join(path, afile))))
+            #files.append()
 
     # print "Files: " + str(files)
     # print "SubFolders are: " + str(subfolders)
@@ -32,16 +48,16 @@ if len(sys.argv) != 2:
     print "Program usage is: mergefolders.py path"
     exit()
 
-print loadFolder(os.path.dirname(os.path.realpath(sys.argv[1])))
+outputFileName = os.path.join(os.path.dirname(os.path.realpath(sys.argv[1])), "mergefolder.txt")
+#if os.path.exists(outputFileName):
+#    outputFileName.join(1);
 
-BLOCKSIZE = 65536
-hasher = hashlib.sha1()
-with open('test1', 'rb') as afile:
-    buf = afile.read(BLOCKSIZE)
-    while len(buf) > 0:
-        hasher.update(buf)
-        buf = afile.read(BLOCKSIZE)
-print(hasher.hexdigest())
+print outputFileName
+
+print str(os.listdir(os.path.realpath(sys.argv[1])))
+with open(outputFileName, 'w') as outputFile:
+    loadFolder(os.path.realpath(sys.argv[1]), outputFile)
+
 
 md = hashlib.sha1()
 md.update("Some text message")
